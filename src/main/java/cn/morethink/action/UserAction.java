465132e401a4d347553ec9b74791530db84fee50
morethink <lwhyx9@qq.com>
2017-12-12 16:48:35 +0800

    增加异常处理

--- src/main/java/cn/morethink/action/UserAction.java
+++ src/main/java/cn/morethink/action/UserAction.java
@@ -3,9 +3,13 @@ package cn.morethink.action;
 import cn.morethink.eneity.User;
 import cn.morethink.util.Result;
 import cn.morethink.util.ResultUtil;
+import org.apache.commons.io.FileUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -18,9 +22,13 @@ import java.util.List;
 public class UserAction {
 
     @PostMapping
-    public Result saveUsers(@RequestBody  List<User> users) {
-        return ResultUtil.successResult(users);
+    public Result saveUser(@ModelAttribute User user) {
+        return ResultUtil.successResult(user);
+    }
 
+    @PostMapping("saveUsers")
+    public Result saveUsers(@RequestBody List<User> users) {
+        return ResultUtil.successResult(users);
     }
 
     @GetMapping("/{id}")
@@ -64,6 +72,14 @@ public class UserAction {
     public Result testModelAttribute() {
 
         return ResultUtil.successResult("");
+    }
+
+    @RequestMapping("/doUpload")
+    public Result doUpload(@RequestParam("file") MultipartFile file) throws IOException {
+        if (!file.isEmpty()) {
+            FileUtils.copyInputStreamToFile(file.getInputStream(), new File("E://", file.getOriginalFilename()));
+        }
+        return ResultUtil.SUCCESS_RESULT;
     }
 
 }
